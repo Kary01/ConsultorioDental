4bf2eaeb7e59a014ff85cf171e88124089af45ad
kary <anakaren.ruizb@gmail.com>
2019-11-28 14:56:48 -0600

    principal

---
+++ Principal Consultorio/enviar.php
@@ -0,0 +1,28 @@
+<?php
+
+    //LLAMANDO A LOS CAMPOS
+
+    $tratamiento = $_POST['tratamiento'];
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $telefono = $_POST['telefono'];
+
+    //DATOS PARA EL CORREO
+
+    $destinatario = "Consultorio.Dental0001@gmail.com";
+    $asunto = "Citas desde Web";
+
+    $carta = "De: $nombre \n";
+    $carta .= "Correo: $email \n";
+    $carta .= "Teléfono: $telefono \n";
+    $carta .= "Fecha: $fecha \n";
+    $carta .= "Hora: $hora \n";
+    $carta .= "Tratamiento: $tratamiento";
+
+    //ENVIANDO MENSAJE
+
+    mail($destinatario, $asunto, $carta);
+
+?>
